da169bd611f6c82878708628ad7f5f440376627b
Jon Anderson <jon1992@gmail.com>
2016-05-14 22:53:36 -0500

    Got a working minitest test

--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class TodosControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def test_index
+    IndexTests.new.all_stub do |samples|
+      get :index
+
+      samples.each do |s|
+        assert_select('li h1', s.name)
+        assert_select('li p', s.description)
+      end
+
+      assert_select('li p', 'Completed')
+    end
+  end
+
+end
+
+class IndexTests
+
+  def all_stub
+    sample_todo = Todo.new(
+      name: '1',
+      description: 'description 1',
+      completed: true
+    )
+    sample_todo_2 = Todo.new(
+      name: '2',
+      description: 'description 2'
+    )
+    samples = [sample_todo, sample_todo_2]
+
+    Todo.stub :all, samples do
+      yield samples
+    end
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require 'minitest/mock'
+
 module ActiveSupport
   class TestCase
     fixtures :all
